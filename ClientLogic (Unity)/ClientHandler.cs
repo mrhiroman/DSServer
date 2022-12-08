52f36df2bfece0f911d4ec0ed994081620519638
h1roman <hiromanosu@gmail.com>
2022-12-08 23:04:40 +0300

    added code from Unity

---
+++ ClientLogic (Unity)/ClientHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using UnityEngine;
+
+public class ClientHandler : MonoBehaviour
+{
+    public static void Welcome(Packet _packet)
+    {
+        string _msg = _packet.ReadString();
+        int _myId = _packet.ReadInt();
+
+        Debug.Log($"Message from server: {_msg}");
+        Client.Instance.myId = _myId;
+
+        ClientSend.WelcomeReceived();
+
+        Client.Instance.udp.Connect(((IPEndPoint)Client.Instance.tcp.socket.Client.LocalEndPoint).Port);
+    }
+}
---
+++ ClientLogic (Unity)/ClientSend.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientSend : MonoBehaviour
+{
+    private static void SendTCPData(Packet _packet)
+    {
+        _packet.WriteLength();
+        Client.Instance.tcp.SendData(_packet);
+    }
+
+    private static void SendUDPData(Packet _packet)
+    {
+        _packet.WriteLength();
+        Client.Instance.udp.SendData(_packet);
+    }
+
+    #region packets
+    public static void WelcomeReceived()
+    {
+        using(Packet _packet = new Packet((int)ClientPackets.welcomeReceived))
+        {
+            _packet.Write(Client.Instance.myId);
+            _packet.Write(UIManager.Instance.usernameField.text);
+            SendTCPData(_packet);
+        }
+    }
+    #endregion
+}
